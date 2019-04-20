3ae7ae0cda876d5fd4c258a1643e436a000d77b6
Torsten Schmits <torstenschmits@gmail.com>
2019-04-20 13:53:31 +0200

    `modifyL`, `getL`

--- lib/Control/Monad/DeepState.hs
+++ lib/Control/Monad/DeepState.hs
@@ -1,7 +1,7 @@
 module Control.Monad.DeepState where
 
 import Control.Lens (Lens')
-import qualified Control.Lens as Lens (set, view)
+import qualified Control.Lens as Lens (over, set, view, views)
 import Control.Monad.State.Class (MonadState)
 import qualified Control.Monad.State.Class as MS (MonadState(get), modify)
 
@@ -36,9 +36,17 @@ modify :: ∀ s' s m. MonadDeepState s s' m => (s' -> s') -> m ()
 modify f =
   modifyM (pure . f)
 
-getsL :: ∀ s' s m a. MonadDeepState s s' m => Lens' s' a -> m a
-getsL =
+modifyL :: ∀ s' s a m. MonadDeepState s s' m => Lens' s' a -> (a -> a) -> m ()
+modifyL lens f =
+  modify $ Lens.over lens f
+
+getL :: ∀ s' s m a. MonadDeepState s s' m => Lens' s' a -> m a
+getL =
   gets . Lens.view
+
+getsL :: ∀ s' s m a b. MonadDeepState s s' m => Lens' s' a -> (a -> b) -> m b
+getsL lens =
+  gets . Lens.views lens
 
 setL :: ∀ s' s m a. MonadDeepState s s' m => Lens' s' a -> a -> m ()
 setL lens =
